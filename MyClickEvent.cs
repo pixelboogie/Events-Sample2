d9abdc92eef8989e584b9c2029c50edb3323c11f
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-12 17:52:19 -0800

    Add files via upload

---
+++ MyClickEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class MyClickEvent : MonoBehaviour
+{
+
+    private Collider collider;
+
+    public delegate void ClickAction();
+    public static event ClickAction OnAnythingClicked;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(collider.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                OnAnythingClicked();
+            }
+        }
+    }
+
+
+
+}
---
+++ MyTeleportScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyTeleportScript : MonoBehaviour 
+{
+    void OnEnable()
+    {
+        MyClickEvent.OnAnythingClicked += Teleport;
+    }
+
+
+    void OnDisable()
+    {
+        MyClickEvent.OnAnythingClicked -= Teleport;
+    }
+
+
+    void Teleport()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Random.Range(1.0f, 3.0f);
+        transform.position = pos;
+    }
+}
